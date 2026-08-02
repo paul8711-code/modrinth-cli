cbd0ccc00f2e04475ea4cb0b42aa5c4fc805af2f
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-08-02 13:14:07 +0200

    feat: create sample program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+const USER_AGENT: &str = concat!(
+    "paul8711-code/",
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("CARGO_PKG_AUTHORS"),
+    ")"
+);
+
+const API: &str = "https://api.modrinth.com";
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", USER_AGENT);
+    let client = reqwest::blocking::Client::builder()
+        .user_agent(USER_AGENT)
+        .build()
+        .unwrap();
+    let response = client
+        .get(format!("{}/v2/search", API))
+        // search for create mod and only return the first result
+        .query(&[("query", "Create"), ("limit", "1")])
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    println!("{:?}", response);
 }
